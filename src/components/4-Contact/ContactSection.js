66f11994179ec92b261a6fab5ab4c9451e122d14
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-02-26 11:22:20 -0300

    Contact section update

---
+++ src/components/4-Contact/ContactSection.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import styles from "./ContactSection.module.scss";
+import { useInView } from "react-intersection-observer";
+
+const ContactSection = () => {
+  const { t, i18n } = useTranslation();
+
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+  });
+
+  return (
+    <Container ref={ref} className={`${styles.myWorkBackContainer}`} fluid>
+      <Container className={`${styles.myWorkContainer}`}>
+        <div className={inView ? `${styles.backgroundShape} slide-right-3` : `${styles.backgroundShape} invisible`} />
+        <Row
+          className={
+            inView
+              ? `${styles.aniamatedH2} slide-right-3 mx-auto`
+              : `mx-auto invisible`
+          }
+        >
+          <h2>{t("myWorkTitle")}</h2>
+        </Row>
+      </Container>
+    </Container>
+  );
+};
+
+export default ContactSection;
