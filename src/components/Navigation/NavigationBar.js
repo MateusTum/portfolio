3810825667e18e82f5928b9306e89523da0d81b7
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-02-26 13:06:36 -0300

    Update navbar styles

--- src/components/Navigation/NavigationBar.js
+++ src/components/Navigation/NavigationBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import { Navbar, Nav, NavDropdown, Image } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
@@ -9,10 +9,10 @@ function NavigationBar() {
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
-    if (localStorage.getItem("language"))  {
-      i18n.changeLanguage(localStorage.getItem("language"))
+    if (localStorage.getItem("language")) {
+      i18n.changeLanguage(localStorage.getItem("language"));
     } else {
-      i18n.changeLanguage("en")
+      i18n.changeLanguage("en");
     }
   }, []); // The empty array ensures this effect runs only once after initial mount
 
@@ -48,48 +48,60 @@ function NavigationBar() {
 
   return (
     <header>
-      <Navbar data-bs-theme="dark" className={`${styles.navbar} p-0`} fixed="top" >
+      <Navbar
+        expand="lg"
+        data-bs-theme="dark"
+        className={`${styles.navbar} p-1`}
+        fixed="top"
+      >
         <Container>
-          <Nav className="ms-auto">
-            <Nav.Link as={Link} className={`${styles.navlink}`} to="/">
-              {t("Navbar.Home")}
-            </Nav.Link>
-            <Nav.Link as={Link} className={`${styles.navlink}`} to="/">
-              {t("Navbar.About")}
-            </Nav.Link>
-            <Nav.Link as={Link} className={`${styles.navlink}`} to="/projects">
-              {t("Navbar.Portfolio")}
-            </Nav.Link>
-            <Nav.Link as={Link} className={`${styles.navlink}`} to="/">
-              {t("Navbar.MyWork")}
-            </Nav.Link>
-            <Nav.Link as={Link} className={`${styles.navlink}`} to="/">
-              {t("Navbar.Contact")}
-            </Nav.Link>
-          </Nav>
-          <NavDropdown
-            className="ms-auto"
-            title={getLanguageNameWithFlag(i18n.language)}
-          >
-            <NavDropdown.Item onClick={() => changeLanguage("en")}>
-              <Image
-                src={`${process.env.PUBLIC_URL}/images/Flag_of_United_States.png`}
-                width={20}
-                height={20}
-                alt="Brazilian Flag"
-              />{" "}
-              English
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => changeLanguage("pt")}>
-              <Image
-                src={`${process.env.PUBLIC_URL}/images/Flag_of_Brazil.png`}
-                width={20}
-                height={20}
-                alt="USA Flag"
-              />{" "}
-              Português
-            </NavDropdown.Item>
-          </NavDropdown>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-auto"/>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} className={`${styles.navlink}`} to="/">
+                {t("Navbar.Home")}
+              </Nav.Link>
+              <Nav.Link as={Link} className={`${styles.navlink}`} to="/">
+                {t("Navbar.About")}
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                className={`${styles.navlink}`}
+                to="/projects"
+              >
+                {t("Navbar.Portfolio")}
+              </Nav.Link>
+              <Nav.Link as={Link} className={`${styles.navlink}`} to="/">
+                {t("Navbar.MyWork")}
+              </Nav.Link>
+              <Nav.Link as={Link} className={`${styles.navlink}`} to="/">
+                {t("Navbar.Contact")}
+              </Nav.Link>
+            </Nav>
+            <NavDropdown
+              className={`ms-auto`}
+              title={getLanguageNameWithFlag(i18n.language)}
+            >
+              <NavDropdown.Item onClick={() => changeLanguage("en")}>
+                <Image
+                  src={`${process.env.PUBLIC_URL}/images/Flag_of_United_States.png`}
+                  width={20}
+                  height={20}
+                  alt="Brazilian Flag"
+                />{" "}
+                English
+              </NavDropdown.Item>
+              <NavDropdown.Item onClick={() => changeLanguage("pt")}>
+                <Image
+                  src={`${process.env.PUBLIC_URL}/images/Flag_of_Brazil.png`}
+                  width={20}
+                  height={20}
+                  alt="USA Flag"
+                />{" "}
+                Português
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
