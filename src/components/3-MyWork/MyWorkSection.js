6e03a491fe73d30b5420af10f0a6b1eba7d082de
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-02-26 11:22:13 -0300

    MyWork section update

---
+++ src/components/3-MyWork/MyWorkSection.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import styles from "./MyWork.module.scss";
+import { useInView } from "react-intersection-observer";
+
+const CustomTitle = ({ inView }) => {
+  const { t, i18n } = useTranslation();
+  const [isClicked, setIsClicked] = useState(false);
+
+  // Function to toggle the isClicked state
+  const handleRowClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  return (
+    <>
+      <div
+        className={
+          isClicked
+            ? `${styles.transformX50} ${styles.tab}`
+            : `${styles.tab} ${styles.transformX0}`
+        }
+        onClick={handleRowClick}
+      >
+        <h2>{t("MyWork.Title")}</h2>
+      </div>
+    </>
+  );
+};
+
+const ProjectsSection = () => {
+  const { t, i18n } = useTranslation();
+
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+    triggerOnce: true,
+  });
+
+  return (
+    <Container ref={ref} className={`${styles.myWorkBackContainer}`} fluid>
+      <div className={inView ? `slideTitleAnimation` : `hidden`}>
+        <CustomTitle />
+      </div>
+
+      <Container className={`${styles.myWorkContainer}`}></Container>
+    </Container>
+  );
+};
+
+export default ProjectsSection;
