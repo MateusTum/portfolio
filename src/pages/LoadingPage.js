f7563a0cd718f7e3ce2c63309f77517dda19c642
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-03-02 15:40:42 -0300

    Update LoadingPage

--- src/pages/LoadingPage.js
+++ src/pages/LoadingPage.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import CloudsSvg from "../components/SVGS/Clouds";
+import './Loading.scss';
 
 const LoadingPage = () => {
   return (
-    <Container fluid>
+    <Container fluid className="mx-0 p-0">
+      <div class="terminal-loader">
+          <div class="terminal-header">
+            <div class="terminal-title">Status</div>
+            <div class="terminal-controls">
+              <div class="control close"></div>
+              <div class="control minimize"></div>
+              <div class="control maximize"></div>
+            </div>
+          </div>
+          <div class="text">Loading...</div>
+        </div>
       <Row>
         <Col className="text-center">
         <CloudsSvg />
-          <h1 className="loadingH1">Loading page</h1>
         </Col>
       </Row>
     </Container>
