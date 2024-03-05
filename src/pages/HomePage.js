c85b0de64c979b10e9d65f0de64f00b725e148ee
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-03-05 12:41:18 -0300

    Update HomePage.js- Add concepts

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,8 +2,7 @@ import React from "react";
 import PresentationSection from "../components/0-Presentation/PresentationSection";
 import AboutSection from "../components/1-About/AboutSection";
 import PortfolioSection from "../components/2-Portfolio/PortfolioSection";
-// import MyWorkSection from "../components/3-MyWork/MyWorkSection";
-import Footer from "../components/Footer/Footer";
+import ConceptsSection from "../components/3-Concepts/ConceptsSection";
 
 const Home = () => {
   return (
@@ -11,8 +10,7 @@ const Home = () => {
       <PresentationSection />
       <AboutSection />
       <PortfolioSection />
-      {/* <MyWorkSection /> */}
-      <Footer />
+      <ConceptsSection />
     </>
   );
 };
