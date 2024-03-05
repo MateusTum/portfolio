affc48fccc3a8a0036086043ce297c668c00b955
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-03-05 12:43:38 -0300

    Comment out ConceptsSection - not ready yet

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import PresentationSection from "../components/0-Presentation/PresentationSection";
 import AboutSection from "../components/1-About/AboutSection";
 import PortfolioSection from "../components/2-Portfolio/PortfolioSection";
-import ConceptsSection from "../components/3-Concepts/ConceptsSection";
+// import ConceptsSection from "../components/3-Concepts/ConceptsSection";
 
 const Home = () => {
   return (
@@ -10,7 +10,7 @@ const Home = () => {
       <PresentationSection />
       <AboutSection />
       <PortfolioSection />
-      <ConceptsSection />
+      {/* <ConceptsSection /> */}
     </>
   );
 };
